7a9989c6702f300bf85f9d23eecd49bb192779b3
ferzy11 <ferzy11@gmail.com>
2018-06-12 22:50:01 -0500

    Add files via upload

---
+++ Assignment 5/cmake-build-debug/CMakeFiles/Assignment_5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment_5.dir/main.cpp.obj"
+  "Assignment_5.pdb"
+  "Assignment_5.exe"
+  "Assignment_5.exe.manifest"
+  "libAssignment_5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
